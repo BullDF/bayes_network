d70a6731fafcde233fc8aba2ad17f4ef169536f7
Johnny Meng <jmyw139@gmail.com>
2025-03-06 00:21:38 -0500

    Created distribution file

---
+++ distribution.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from typing import Any, Callable, Optional
+
+
+class Distribution(ABC):
+    domain: Any
+    probability_function: Optional[Callable]
+
+    def __init__(self, domain: Any):
+        self.domain = domain
+        self.probability_function = None
+
+    def set_probability_function(self, probability_function: Callable) -> None:
+        self.probability_function = probability_function
+
+
+class DiscreteDistribution(Distribution):
+    pass
+
+
+class ContinuousDistribution(Distribution):
+    domain: tuple[float, float]
+
+
