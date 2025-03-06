a60c6bdaf3fbdf3e31f805d7fac9c30dddb8d8ab
Johnny Meng <jmyw139@gmail.com>
2025-03-06 00:21:32 -0500

    Finished bayes_network

--- bayes_network.py
+++ bayes_network.py
@@ -1,30 +1,20 @@
-from typing import List, Self
-from abc import ABC
+from typing import Self, Any, Optional, Union
+from abc import ABC, abstractmethod
+from distribution import Distribution, DiscreteDistribution, ContinuousDistribution
 
 
-class Distribution(ABC):
-    pass
-
-
-class DiscreteDistribution(Distribution):
-    pass
-
-
-class ContinuousDistribution(Distribution):
-    pass
-
-
-class Node:
+class Node(ABC):
     name: str
-    parents: List[Self]
-    children: List[Self]
-    distribution: Distribution
+    parents: list[Self]
+    children: list[Self]
+    domain: Any
+    distributions: dict[tuple[tuple[str, Any]], Distribution]
 
     def __init__(self, name: str) -> None:
         self.name = name
         self.parents = []
         self.children = []
-        self.distribution = None
+        self.distributions = None
 
     def add_parent(self, parent: Self) -> None:
         self.parents.append(parent)
@@ -32,15 +22,95 @@ class Node:
     def add_child(self, child: Self) -> None:
         self.children.append(child)
 
-    def get_parents(self) -> List[Self]:
+    def get_parents(self) -> list[Self]:
         return self.parents
 
-    def get_children(self) -> List[Self]:
+    def get_children(self) -> list[Self]:
         return self.children
+
+    @abstractmethod
+    def get_domain(self) -> Union[set, tuple[float, float]]:
+        pass
+
+    @abstractmethod
+    def _check_distributions(self, distributions: dict[tuple[tuple[str, Any]], Distribution]) -> None:
+        pass
+
+    @abstractmethod
+    def is_in_domain(self, value: Any) -> bool:
+        pass
+
+    def set_distributions(self, distributions: dict[tuple[tuple[str, Any]], Distribution]) -> None:
+        self._check_distributions(distributions)
+        self.distributions = distributions
+
+
+class DiscreteNode(Node):
+    domain: set
+
+    def __init__(self, name: str, domain: set) -> None:
+        super().__init__(name)
+        self.domain = domain
+
+    def is_in_domain(self, value: Any) -> bool:
+        return value in self.domain
+
+    def _check_distributions(self, distributions: dict[tuple[tuple[str, Any]], Distribution]) -> None:
+        for conditions, distribution in distributions.items():
+            if not isinstance(distribution, DiscreteDistribution):
+                raise ValueError(
+                    f'DiscreteNode {self.name} can only have DiscreteDistribution.')
+            if distribution.domain != self.domain:
+                raise ValueError(
+                    f'Distribution domain {distribution.domain} does not match node domain {self.domain}.')
+            for variable, value in conditions:
+                parent = None
+                for p in self.parents:
+                    if p.name == variable:
+                        parent = p
+                        break
+                if parent is None:
+                    raise ValueError(
+                        f'Variable {variable} is not a parent of node {self.name}.')
+                if not parent.is_in_domain(value):
+                    raise ValueError(
+                        f'Value {value} is not in the domain of parent {variable}.')
+
+
+class ContinuousNode(Node):
+    domain: tuple[float, float]
+
+    def __init__(self, name: str, domain: tuple[float, float]) -> None:
+        super().__init__(name)
+        self.domain = domain
+
+    def is_in_domain(self, value: Any) -> bool:
+        return self.domain[0] < value < self.domain[1]
+
+    def _check_distributions(self, distributions: dict[tuple[tuple[str, Any]], Distribution]) -> None:
+        for conditions, distribution in distributions.items():
+            if not isinstance(distribution, ContinuousDistribution):
+                raise ValueError(
+                    f'ContinuousNode {self.name} can only have ContinuousDistribution.')
+            if distribution.domain != self.domain:
+                raise ValueError(
+                    f'Distribution domain {distribution.domain} does not match node domain {self.domain}.')
+            for variable, value in conditions:
+                parent = None
+                for p in self.parents:
+                    if p.name == variable:
+                        parent = p
+                        break
+                if parent is None:
+                    raise ValueError(
+                        f'Variable {variable} is not a parent of node {self.name}.')
+                if not parent.is_in_domain(value):
+                    raise ValueError(
+                        f'Value {value} is not in the domain of parent {variable}.')
 
 
 class BayesNetwork:
-    nodes: List[Node]
+    nodes: list[Node]
 
     def __init__(self):
         self.nodes = []
