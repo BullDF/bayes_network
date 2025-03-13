f8cd190252a23bcc670982a210e0bfe0f73d8538
Johnny Meng <jmyw139@gmail.com>
2025-03-13 18:06:56 -0400

    Finished computing joint probability

--- bayes_network.py
+++ bayes_network.py
@@ -1,4 +1,5 @@
 from vertex import Vertex
+from typing import Any
 
 
 class BayesNetwork:
@@ -22,5 +23,35 @@ class BayesNetwork:
     def find_roots(self) -> list[Vertex]:
         return [vertex for vertex in self.vertices.values() if not vertex.parents]
 
-    def joint_probability(self):
-        pass
+    def __call__(self, *args) -> float:
+        if len(args) != 1:
+            raise ValueError('Computing joint probability requires exactly one argument.')
+        values = args[0]
+        if not isinstance(values, dict):
+            raise ValueError('Values must be provided as a dictionary.')
+        if len(values) != len(self.vertices):
+            raise ValueError('Length of values must match number of vertices.')
+        
+        vertices = self.find_roots()
+        seen = set(vertices)
+        prob = 1
+        
+        while vertices:
+            curr = vertices.pop()
+            if curr.name not in values:
+                raise ValueError(f'No value provided for vertex {curr.name}.')
+            
+            if not curr.parents:
+                prob *= curr(values[curr.name])
+            else:
+                conditions = {}
+                for parent in curr.parents:
+                    conditions[parent] = values[parent]
+                prob *= curr(values[curr.name], conditions)
+
+            for child in curr.children.values():
+                if child not in seen:
+                    vertices.append(child)
+                    seen.add(child)
+
+        return prob
