232d77058945983aa93ddc3d73ea18e0ce092854
Johnny Meng <jmyw139@gmail.com>
2025-03-13 00:52:30 -0400

    Added checks

--- vertex.py
+++ vertex.py
@@ -21,12 +21,16 @@ class Vertex:
             raise ValueError('Cannot add parent to vertex with distribution.')
         if parent.name in self.parents:
             raise ValueError(f'Vertex {parent.name} is already a parent of vertex {self.name}.')
+        if parent.name in self.children:
+            raise ValueError(f'Vertex {parent.name} is already a child of vertex {self.name}.')
         
         self.parents[parent.name] = parent
 
     def add_child(self, child: Self) -> None:
         if child.name in self.children:
             raise ValueError(f'Vertex {child.name} is already a child of vertex {self.name}.')
+        if child.name in self.parents:
+            raise ValueError(f'Vertex {child.name} is already a parent of vertex {self.name}.')
         self.children[child.name] = child
 
     def in_domain(self, value: Any) -> bool:
