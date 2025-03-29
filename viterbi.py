50fe2019821ba101a828e4f8a94f3bb8721ef4ff
Johnny Meng <jmyw139@gmail.com>
2025-03-29 15:52:25 -0400

    Used Distribution.__call__

--- viterbi.py
+++ viterbi.py
@@ -12,7 +12,7 @@ def viterbi_base(hmm: HiddenMarkovModel, initial_obs: Any) -> tuple[Any, float]:
 
     probs = {}
     for initial_hidden in hmm.hidden_domain:
-        probs[initial_hidden] = hmm.initial_distribution.distribution[initial_hidden] * hmm.emission_distribution.distributions[frozenset([f'Zt: {initial_hidden}'])].distribution[initial_obs]
+        probs[initial_hidden] = hmm.initial_distribution(initial_hidden) * hmm.emission_distribution(initial_obs, {'Zt': initial_hidden})
 
     max_value = max(probs, key=probs.get)
     normalized_probs = normalize(probs)
@@ -25,8 +25,8 @@ def viterbi_step(hmm: HiddenMarkovModel, prev_hidden: Any, prev_prob: float, cur
     
     probs = {}
     for curr_hidden in hmm.hidden_domain:
-        transition_prob = hmm.transition_distribution.distributions[frozenset([f'Zt-1: {prev_hidden}'])].distribution[curr_hidden]
-        emission_prob = hmm.emission_distribution.distributions[frozenset([f'Zt: {curr_hidden}'])].distribution[curr_obs]
+        transition_prob = hmm.transition_distribution(curr_hidden, {'Zt-1': prev_hidden})
+        emission_prob = hmm.emission_distribution(curr_obs, {'Zt': curr_hidden})
         probs[curr_hidden] = prev_prob * transition_prob * emission_prob
     max_value = max(probs, key=probs.get)
     normalized_probs = normalize(probs)
@@ -48,4 +48,5 @@ def viterbi(hmm: HiddenMarkovModel, observations: list) -> list:
 
 
 if __name__ == "__main__":
-    pass
+    hmm = read_hmm_from_txt('hmm_ex.txt')
+    print(viterbi(hmm, [1, 0, 1, 0, 0]))
